7f9a80e11f96cdcb9aed7ca925285f553e6fcf6f
Louise Storegaard Nielsen <louisestoregaard@gmail.com>
2024-07-25 15:16:16 +0200

    Added favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="h-screen bg-leaves flex justify-center items-center">
-      <div className="bg-black opacity-30 h-screen w-screen absolute"></div>
+      <div className="bg-black opacity-50 h-screen w-screen absolute"></div>
       <div className="absolute">
-        <img src="/light_w_undertext.svg" className="h-28" />
+        <Image src="/light_w_undertext.svg" alt="Logo" width={"500"} height={"300"} className="h-28 logoAnimation" />
       </div>
     </main>
   );
