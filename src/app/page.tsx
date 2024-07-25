3b411f3e6f01122c2d892aad13260319a0102364
Louise Storegaard Nielsen <louisestoregaard@gmail.com>
2024-07-25 15:18:57 +0200

    Animation name edited

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="h-screen bg-leaves flex justify-center items-center">
+    <main className="h-screen bg-leaves flex justify-center items-center fadeInAnimation">
       <div className="bg-black opacity-50 h-screen w-screen absolute"></div>
       <div className="absolute">
-        <Image src="/light_w_undertext.svg" alt="Logo" width={"500"} height={"300"} className="h-28 logoAnimation" />
+        <Image src="/light_w_undertext.svg" alt="Logo" width={"500"} height={"300"} className="h-28 fadeInAnimation" />
       </div>
     </main>
   );
