22801cb4a73b9d0dbaa28b0b93f4ca18f8e2d02f
Louise Storegaard Nielsen <louisestoregaard@gmail.com>
2024-07-25 12:07:22 +0200

    BG image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>Gaard Studio</div>
+    <main className="h-screen bg-leaves flex justify-center items-center">
+      <div className="bg-black opacity-30 h-screen w-screen absolute"></div>
+      <div className="absolute">
+        <img src="/light_w_undertext.svg" className="h-28" />
+      </div>
     </main>
   );
 }
