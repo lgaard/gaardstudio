bab968236cd2e645c8512194699cf3c36dc227fc
Louise Storegaard Nielsen <louisestoregaard@gmail.com>
2024-08-05 20:27:02 +0200

    New opacity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="h-screen bg-leaves flex justify-center items-center fadeInAnimation">
-      <div className="bg-black opacity-50 h-screen w-screen absolute"></div>
+      <div className="bg-black opacity-90 h-screen w-screen absolute"></div>
       <div className="absolute">
         <Image src="/light_w_undertext.svg" alt="Logo" width={"500"} height={"300"} className="h-28 fadeInAnimation" />
       </div>
