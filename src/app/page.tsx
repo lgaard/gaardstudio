99fecb59ee3c49913c3e7433e57c7a6db878050d
Louise Storegaard Nielsen <louisestoregaard@gmail.com>
2024-08-15 20:18:45 +0200

    New bg and favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="h-screen bg-leaves flex justify-center items-center fadeInAnimation">
-      <div className="bg-black opacity-90 h-screen w-screen absolute"></div>
+    <main className="h-screen bg-sand flex justify-center items-center">
+      {/* <div className="bg-sand opacity-90 h-screen w-screen absolute"></div> */}
       <div className="absolute">
-        <Image src="/light_w_undertext.svg" alt="Logo" width={"500"} height={"300"} className="h-28 fadeInAnimation" />
+        <Image src="/dark_w_undertext.svg" alt="Logo" width={"400"} height={"250"} className="sm:h-20 h-14" />
       </div>
     </main>
   );
